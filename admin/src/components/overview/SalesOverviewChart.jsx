bab1d7c76aa1e929cbe3b59c02a3c30ef7d8988b
Abhyuday <apsworks1212@gmail.com>
2024-09-18 00:28:28 +0530

    new admin pannel added

---
+++ admin/src/components/overview/SalesOverviewChart.jsx
@@ -0,0 +1,66 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { motion } from "framer-motion";
+
+const salesData = [
+	{ name: "Jan", sales: 200 },
+  { name: "Jul", sales: 350 },
+  { name: "Aug", sales: 380 },
+  { name: "Sep", sales: 500 },
+  { name: "Oct", sales: 400 },
+  { name: "Nov", sales: 490 },
+  { name: "Dec", sales: 720 },
+  { name: "Feb", sales: 590 },
+  { name: "Mar", sales: 680 },
+  { name: "Apr", sales: 630 },
+  { name: "May", sales: 710 },
+  { name: "Jun", sales: 750 },
+];
+
+const SalesOverviewChart = () => {
+  return (
+    <motion.div
+      className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+    >
+      <h2 className="text-lg font-medium mb-4 text-gray-100">
+        Applications Submission Overview
+      </h2>
+
+      <div className="h-80">
+        <ResponsiveContainer width={"100%"} height={"100%"}>
+          <LineChart data={salesData}>
+            <CartesianGrid strokeDasharray="3 3" stroke="#4B5563" />
+            <XAxis dataKey={"name"} stroke="#9ca3af" />
+            <YAxis stroke="#9ca3af" />
+            <Tooltip
+              contentStyle={{
+                backgroundColor: "rgba(31, 41, 55, 0.8)",
+                borderColor: "#4B5563",
+              }}
+              itemStyle={{ color: "#E5E7EB" }}
+            />
+            <Line
+              type="monotone"
+              dataKey="sales"
+              stroke="#6366F1"
+              strokeWidth={3}
+              dot={{ fill: "#6366F1", strokeWidth: 2, r: 6 }}
+              activeDot={{ r: 8, strokeWidth: 2 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </motion.div>
+  );
+};
+export default SalesOverviewChart;
